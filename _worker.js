30c6949ebb7ea56600179e129e3623c816550950
Ahmad2213 <106820058+Ahmad-2213@users.noreply.github.com>
2024-07-06 20:06:05 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -699,7 +699,7 @@ const ed = 'RUR0dW5uZWw=';
  * @returns {string}
  */
 function getวเลสConfig(userIDs, hostName) {
-	const commonUrlPart = `:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D443#${hostName}`;
+	const commonUrlPart = `:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D10000#${hostName}`;
 	const hashSeparator = "################################################################";
 
 	// Split the userIDs into an array
