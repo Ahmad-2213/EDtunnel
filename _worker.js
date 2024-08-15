6c9db662e1c04d75a50bf2eb8231cc12cbb0e306
Ahmad2213 <106820058+Ahmad-2213@users.noreply.github.com>
2024-08-15 19:16:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '43e8276e-104b-45d7-9dac-b8a1fc7c2a24';
 
-const พร็อกซีไอพีs = ['143.47.227.23'];
+const พร็อกซีไอพีs = ['62.3.12.185'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
