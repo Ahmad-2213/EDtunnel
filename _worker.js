6c6f79e32df250a812f7cc27e8f589364df359fb
Ahmad2213 <106820058+Ahmad-2213@users.noreply.github.com>
2024-11-21 16:53:40 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -149,7 +149,7 @@ async function วเลสOverWSHandler(request) {
 	const webSocketPair = new WebSocketPair();
 	const [client, webSocket] = Object.values(webSocketPair);
 	webSocket.accept({
-maxPayload: 1048576, // Increase the maximum payload size
+maxPayload: 128000, // Increase the maximum payload size 1048576
 
 });
 
